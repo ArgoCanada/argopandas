aea8c8a9b18f7e0eb6439a5d5350504253012835
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-06-15 16:59:46 -0300

    more progress on lazy index

--- argodata/index.py
+++ argodata/index.py
@@ -5,14 +5,12 @@ from collections import deque
 
 class ArgoIndex:
 
-    def __init__(self, src, filters=None, start=0, limit=-1):
+    def __init__(self, src, filters=None, start=0, limit=None):
         self._src = str(src)
         self._filters = [] if filters is None else list(filters)
         self._start = int(start)
-        self._limit = int(limit)
-    
-    def _open(self):
-        return gzip.open(self._src)
+        self._limit = limit
+        self._cached_len = None
     
     def is_valid(self):
         try:
@@ -32,7 +30,64 @@ class ArgoIndex:
         except Exception as e:
             raise ValueError(f"Failed to open '{ self._src }': { str(e) }")
 
+    def __getitem__(self, k):
+        if isinstance(k, slice):
+            k_start = k.start
+            k_stop = k.stop
+            if k.step is not None:
+                raise ValueError("Can't subset ArgoIndex with stepped slice")
+
+            if k_start is None and k_stop is None:
+                return self
+
+            if k_start is None:
+                k_start = 0
+
+            # a common case where we can avoid calculating the length
+            if k_stop is None and self._limit is None and k_start >= 0:
+                return ArgoIndex(self._src, filters=self._filters, start=self._start + k_start)
+
+            if k_stop is None:
+                k_stop = -1
+
+            if k_start < 0 or k_stop < 0:
+                this_len = len(self)
+                k_start = this_len + k_start + 1 if k_start < 0 else k_start
+                k_stop = this_len + k_stop + 1 if k_stop < 0 else k_stop
+
+            new_start = self._start + k_start
+            new_limit = k_stop - k_start
+            return ArgoIndex(self._src, filters=self._filters, start=new_start, limit=new_limit)
+
+        elif isinstance(k, int):
+            if k < 0:
+                k = len(self) - k
+
+            n = 0
+            for item in self:
+                if n == k:
+                    return item
+                n += 1
+
+            # take this opportunity to cache the length
+            if self._cached_len is None:
+                self._cached_len = n
+            
+            raise IndexError(f"Can't extract item {k} from ArgoIndex of length {n}")
+        else:
+            raise ValueError(f"Can't subset ArgoIndex with object of type '{type(k)}'")
+
+    def __len__(self):
+        if self._cached_len is None:
+            counter = itertools.count()
+            deque(zip(self, counter), maxlen=0)
+            self._cached_len = next(counter)
+        return self._cached_len
+
     def __iter__(self):
+        if self._limit is not None and self._limit <= 0:
+            return
+
         with self._open() as f:
             names = None
             length = -1
@@ -55,6 +110,9 @@ class ArgoIndex:
                 size += 1
                 if size == self._limit:
                     break
+    
+    def _open(self):
+        return gzip.open(self._src)
 
     def __repr__(self) -> str:
         filter_repr = repr(self._filters)
@@ -63,7 +121,8 @@ class ArgoIndex:
     def __str__(self) -> str:
         return repr(self)
 
-    def __len__(self):
-        counter = itertools.count()
-        deque(zip(self, counter), maxlen=0)
-        return next(counter)
+if __name__ == '__main__':
+    ind = ArgoIndex("../argodata/cache-dev/ar_index_global_meta.txt.gz")
+    print(repr(ind))
+    print(len(ind))
+    print(repr(ind[-2:-1]))
--- tests/test_index.py
+++ tests/test_index.py
@@ -14,11 +14,11 @@ class TestArgoIndex(unittest.TestCase):
 
     def test_argo_index_repr(self):
         self.assertEqual(repr(ArgoIndex('src', None, 0, 1)), """ArgoIndex('src', [], 0, 1)""")
-        self.assertEqual(repr(ArgoIndex('src')), """ArgoIndex('src', [], 0, -1)""")
+        self.assertEqual(repr(ArgoIndex('src')), """ArgoIndex('src', [], 0, None)""")
     
     def test_argo_str(self):
         self.assertEqual(str(ArgoIndex('src', None, 0, 1)), """ArgoIndex('src', [], 0, 1)""")
-        self.assertEqual(str(ArgoIndex('src')), """ArgoIndex('src', [], 0, -1)""")
+        self.assertEqual(str(ArgoIndex('src')), """ArgoIndex('src', [], 0, None)""")
 
     def test_index_valid(self):
         self.assertFalse(ArgoIndex("not a file").is_valid())
@@ -34,6 +34,18 @@ class TestArgoIndex(unittest.TestCase):
             self.assertIn('file', item.keys())
             self.assertRegexpMatches(item['file'], '_meta.nc$')
     
+    def test_slice(self):
+        ind = ArgoIndex(self.index_file)
+        self.assertIs(ind[:], ind)
+        self.assertEqual(len(ind[:1]), 1)
+        self.assertEqual(len(ind[1:]), 1)
+        self.assertEqual(len(ind[0:-1]), 2)
+        self.assertEqual(len(ind[-2:-1]), 1)
+    
+    def test_extract(self):
+        ind = ArgoIndex(self.index_file)
+        self.assertEqual(ind[0]["file"], "csio/2900313/2900313_meta.nc")
+        self.assertEqual(ind[1]["file"], "csio/2902746/2902746_meta.nc")
 
 if __name__ == '__main__':
     unittest.main()
