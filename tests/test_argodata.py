9f042bc198701179af5a51a14bc94bc91b90d621
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-06-15 11:06:32 -0300

    try alternate test

--- tests/test_argodata.py
+++ tests/test_argodata.py
@@ -5,7 +5,7 @@ import argodata.argodata as argodata
 class TestHelloWorld(unittest.TestCase):
 
     def test_hello_world(self):
-        self.assertEqual(argodata.hello_world(), "Hello, world!!")
+        self.assertEqual(argodata.hello_world(), "Hello, world!")
 
 if __name__ == '__main__':
     unittest.main()
