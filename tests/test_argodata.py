d56441c84f8c7c44ee4c53c573176f545f0ed7ea
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-06-15 12:20:50 -0300

    add test mirror

--- tests/test_argodata.py
+++ tests/test_argodata.py
@@ -7,5 +7,10 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(argodata.hello_world(), "Hello, world!")
 
+    def test_mirror_exists(self):
+        import os
+        test_dir = os.path.join(os.path.dirname(__file__), "argo-test-mirror")
+        self.assertTrue(os.path.isdir(test_dir))
+
 if __name__ == '__main__':
     unittest.main()
