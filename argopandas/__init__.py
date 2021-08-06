0b5591636e5ce0f115387feb286802ee484a1a85
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-08-06 17:22:59 -0300

    better root package doc

--- argopandas/__init__.py
+++ argopandas/__init__.py
@@ -2,7 +2,60 @@
 The root package contains the interface for
 argopandas.
 
+Global indexes
+--------------
+
+``argopandas.meta``
+
+    The :class:`argopandas.global_index.GlobalMeta` for the current :func:`default_mirror`.
+
+``argopandas.tech``
+
+    The :class:`argopandas.global_index.GlobalTech` for the current :func:`default_mirror`.
+
+``argopandas.prof``
+
+    The :class:`argopandas.global_index.GlobalProf` for the current :func:`default_mirror`.
+
+``argopandas.traj``
+
+    The :class:`argopandas.global_index.GlobalTraj` for the current :func:`default_mirror`.
+
+``argopandas.bio_traj``
+
+    The :class:`argopandas.global_index.GlobalBioTraj` for the current :func:`default_mirror`.
+
+``argopandas.bio_prof``
+
+    The :class:`argopandas.global_index.GlobalBioProf` for the current :func:`default_mirror`.
+
+``argopandas.synthetic_prof``
+
+    The :class:`argopandas.global_index.GlobalSyntheticProf` for the current :func:`default_mirror`.
+
 >>> import argopandas as argo
+>>> argo.meta.head(1)
+                       file  profiler_type institution               date_update
+0  aoml/13857/13857_meta.nc            845          AO 2018-10-11 20:00:14+00:00
+>>> argo.tech.head(1)
+                       file institution               date_update
+0  aoml/13857/13857_tech.nc          AO 2021-04-28 20:03:35+00:00
+>>> argo.prof.head(1)
+                                file                      date  latitude  longitude ocean  profiler_type institution               date_update
+0  aoml/13857/profiles/R13857_001.nc 1997-07-29 20:03:00+00:00     0.267    -16.032     A            845          AO 2018-10-11 18:05:20+00:00
+>>> argo.bio_prof.head(1)
+                                     file                      date  latitude  ...                       parameters parameter_data_mode               date_update
+0  aoml/1900722/profiles/BD1900722_001.nc 2006-10-22 02:16:24+00:00   -40.316  ...  PRES TEMP_DOXY BPHASE_DOXY DOXY                RRRD 2020-03-12 15:32:30+00:00
+[1 rows x 10 columns]
+>>> argo.synthetic_prof.head(1)
+                                     file                      date  latitude  longitude  ... institution           parameters parameter_data_mode               date_update
+0  aoml/1900722/profiles/SD1900722_001.nc 2006-10-22 02:16:24+00:00   -40.316     73.389  ...          AO  PRES TEMP PSAL DOXY                DDDD 2020-10-30 13:30:05+00:00
+[1 rows x 10 columns]
+>>> argo.bio_traj.head(1)
+                             file latitude_max latitude_min  ... institution                                         parameters               date_update
+0  bodc/3901496/3901496_BRtraj.nc         None         None  ...          BO  PRES C1PHASE_DOXY C2PHASE_DOXY TEMP_DOXY DOXY ... 2021-07-22 15:44:18+00:00
+[1 rows x 9 columns]
+
 """
 
 from contextlib import AbstractContextManager
@@ -18,7 +71,7 @@ from .netcdf import NetCDFWrapper
 
 _index_all = global_index.make_globals()
 
-meta = _index_all['meta']
+meta = _index_all['meta']  #: The :class:`argopandas.global_index.GlobalMeta` for the current :func:`default_mirror`.
 tech = _index_all['tech']
 traj = _index_all['traj']
 prof = _index_all['prof']
@@ -199,12 +252,18 @@ def nc(path: Union[str, Iterator[str]]) -> Union[str, Iterator[NetCDFWrapper]]:
     else:
         return _nc_iter(path, mirror)
 
+
 def float(float_id: Union[str, int, Iterator[Union[str, int]]]) -> Union[Float, Iterator[Float]]:
-    mirror = default_mirror()
+    """
+    Subset all the global indexes for a given float.
+
+    :param float_id: A string or int float identifier or iterable
+    """
     if isinstance(float_id, int) or isinstance(float_id, str):
         return Float(float_id, globals=_index_all)
     else:
         return _float_iter(float_id, globals=_index_all)
+
 
 # don't include 'open' in * because it shadows the builtin open()
 __all__ = (
